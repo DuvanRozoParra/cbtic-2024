653b03c2797637f9fe8b5d4317c76d04539378ed
DuvanRozoParra <dduvanrozo@gmail.com>
2024-12-21 00:22:23 -0500

    feat: create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,39 @@
-"use client";
-
 import { ViewSD } from "@layout";
-import { Atom, Swarm } from "@components";
+import { Metadata } from "next";
+import { View3d } from "@components/landing";
+import { Button } from "@/components/ui/button";
+import { Atom } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Unlock the Secrets of the Universe with Physics!",
+};
 
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col gap-8 row-start-2 items-center w-screen h-screen">
+      <main className="flex flex-col gap-8 row-start-2 items-center w-screen h-screen backgroundCosmo">
         <section className="w-screen h-screen relative">
-          <section className="w-full h-full absolute flex flex-col gap-8 row-start-2 items-center">
-            <h1 className="">Crea tu mundo</h1>
+          <section className="w-full h-full absolute flex flex-col gap-8 row-start-2 p-10 md:justify-center">
+            <section className="w-full h-full flex flex-col justify-center gap-2">
+              <h1 className="text-cyan-50 font-roboto font-extrabold text-6xl sm:w-1/2 md:w-1/3">
+                Unlock the Secrets of the Universe with Physics!
+              </h1>
+              <Button className="sm:w-1/3 md:w-1/4 hover:scale-110 inset-full z-50 bg-amber-400 transition-transform hover:transition-transform">
+                <p className="font-roboto text-gray-900 font-semibold text-lg hover:text-cyan-50">
+                  Get Started!
+                </p>
+              </Button>
+            </section>
+            <section className="w-full flex flex-row items-end justify-end">
+              <p className="font-roboto font-semibold text-cyan-50 flex flex-row gap-2">
+                <Atom />
+                CBTIC
+              </p>
+            </section>
           </section>
           <ViewSD>
-            <Atom scale={0.5} />
-            <Swarm count={2000} />
+            <View3d />
           </ViewSD>
         </section>
       </main>
--- src/layout/views/ViewSD.tsx
+++ src/layout/views/ViewSD.tsx
@@ -2,15 +2,15 @@
 
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, Preload, Stats } from "@react-three/drei";
 import {
-  Bloom,
-  EffectComposer,
-  Noise,
-  SMAA,
-  Vignette,
-} from "@react-three/postprocessing";
-import * as THREE from "three";
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+  Preload,
+  Stars,
+  Stats,
+} from "@react-three/drei";
+import { EffectComposer, Noise, SMAA } from "@react-three/postprocessing";
 
 export const ViewSD = ({
   children,
@@ -19,30 +19,36 @@ export const ViewSD = ({
   childrenEffectComposer?: React.ReactNode;
 }>) => {
   return (
-    <Canvas shadows camera={{ fov: 100, position: [0, 0, 5] }}>
+    <Canvas shadows dpr={[1.5, 2]} linear>
       <Stats />
-      <ambientLight intensity={1} />
-      <pointLight
-        intensity={10000}
-        position={[20, 0, 0]}
-        color={new THREE.Color("#000cff")}
-      />
-      <pointLight
-        intensity={10000}
-        position={[-20, 0, 0]}
-        color={new THREE.Color("#ff0000")}
-      />
-
-      <color attach="background" args={["black"]} />
+      <Stars radius={500} depth={50} count={1000} factor={10} />
+      <fog attach="fog" args={["#272730", 16, 30]} />
+      <ambientLight intensity={0.75} />
+      <PerspectiveCamera makeDefault position={[0, 0, 2.5]} fov={75}>
+        <pointLight intensity={10} position={[0, -1, 0]} />
+        <spotLight
+          castShadow
+          intensity={5000}
+          angle={0.2}
+          penumbra={1}
+          position={[0, 20, 0]}
+          shadow-mapSize={[1024, 1024]}
+          shadow-bias={-0.0001}
+        />
+      </PerspectiveCamera>
       <group>{children ?? <></>}</group>
       <Preload all />
-      <OrbitControls />
+      <OrbitControls
+        autoRotate
+        enablePan={false}
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
       <Environment preset="night" backgroundBlurriness={1} />
       <EffectComposer multisampling={0}>
-        <Vignette eskil={false} offset={0.1} darkness={1.2} />
         <Noise opacity={0.03} />
         <SMAA />
-        <Bloom mipmapBlur luminanceThreshold={1} radius={0.6} />
       </EffectComposer>
     </Canvas>
   );
