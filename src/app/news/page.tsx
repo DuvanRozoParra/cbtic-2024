1d36a08b4f782793f31118117e6da279cc5d2163
DuvanRozoParra <dduvanrozo@gmail.com>
2025-03-21 09:16:00 -0500

    feat: fixed others errors

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { NewsLanding } from "@/components/news";
+import { NewsLanding } from "@/components/News";
 
 export const metadata: Metadata = {
   title: "Noticias",
