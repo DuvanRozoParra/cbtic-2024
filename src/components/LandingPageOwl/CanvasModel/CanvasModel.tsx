3235ab8032f90674752b8d701c1c0a7886829d1c
DuvanRozoParra <dduvanrozo@gmail.com>
2025-03-21 09:10:26 -0500

    feat: fixed typed errors

--- src/components/LandingPageOwl/CanvasModel/CanvasModel.tsx
+++ src/components/LandingPageOwl/CanvasModel/CanvasModel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { Suspense } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Model } from "@/models/Rau/owl";
